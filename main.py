7b8e65ac806534a464d71901483d789558837f23
Call-X <emile.miath.architecte@hotmail.com>
2022-11-21 22:29:32 +0100

    uploads

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, request
 
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/",methods=['GET'])
 
 def validation_page():
-    name = str(request.args['miathemileresume'])
+    name = str(request.args['https://miathemileresume.herokuapp.com/'])
     return name
 
 
